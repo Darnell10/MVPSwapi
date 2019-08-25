95eeb4e5841c10d21b0d12f43dd89b800d37fa6e
Darnell10 <DarnellOtto@ac.c4q.nyc>
2019-08-25 13:37:27 -0400

    create retrofit response logic

--- app/src/main/java/com/example/mvpswapi/data/MovieResponse.java
+++ app/src/main/java/com/example/mvpswapi/data/MovieResponse.java
@@ -1,5 +1,16 @@
 package com.example.mvpswapi.data;
 
+import java.util.List;
+
 public class MovieResponse {
 
+    private final List<SWMovie> results;
+
+    public MovieResponse(List<SWMovie> results) {
+        this.results = results;
+    }
+
+    public  List<SWMovie> getMovies(){
+        return results;
+    }
 }
